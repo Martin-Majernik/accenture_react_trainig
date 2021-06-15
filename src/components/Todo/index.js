02fc07c54e2cfd473e0fb3f44775ae5af8c77e4f
Martin-Majernik <majernikmartin95@gmail.com>
2021-06-15 08:10:25 +0200

    delete button

---
+++ src/components/Todo/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export class index extends Component{
+    render() {
+        return(
+            <div>
+                <h1>Todo App</h1>
+            </div>
+            <div>
+            <input type="text"/>
+            <button type="button" > Add To DO </button>
+
+            </div>
+            
+
+           
+         
+        )
+    }
+
+}
